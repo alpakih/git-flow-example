435a0ebcc8897475da71344a14fb2d6c1f5d5455
Ahmad Dikri Alpakih <ahmaddikri13@gmail.com>
2022-11-28 17:55:36 +0700

    feat: create sum calculation func

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/alpakih/git-flow-example/calculation"
 )
 
 func main() {
 
 	fmt.Println("git flow example")
 
+	fmt.Println(fmt.Sprintf("calculate sum = %d", calculation.Sum(1, 1)))
 }
